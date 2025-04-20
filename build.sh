8608c4b672161d18d965fc415f3d5cdd0397db29
RamGai <ram_gai@yahoo.co.in>
2025-04-20 21:48:49 +0530

    tenth

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ if [ "$BRANCH_NAME" == "dev" ]; then
     docker build -t ramgai/dev .
     docker push ramgai/dev:latest
  
-elif [ "$BRANCH_NAME" == "master" ]; then
+elif [ "$BRANCH_NAME" == "main" ]; then
     docker build -t ramgai/prod .
     docker push ramgai/prod:latest
 fi
--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ docker rm my-react-app || true
 if  [ "$BRANCH_NAME" = "dev" ]; then
 docker pull ramgai/dev:latest
 docker run -d -p 80:80 --name my-react-app ramgai/dev:latest
-elif  [ "$BRANCH_NAME" = "master" ]; then
+elif  [ "$BRANCH_NAME" = "main" ]; then
 docker pull ramgai/prod:latest
 docker run -d -p 80:80 --name my-react-app ramgai/prod:latest
 fi
